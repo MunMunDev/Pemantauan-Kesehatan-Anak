fc4b2728b94c870642bd3bb83d14208893d2ee79
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-06-23 13:45:13 +0800

    add perkembangan/aktivitas. from viewmodel, adapter, repository, and add milestone item list

---
+++ app/src/main/java/com/abcd/pemantauankesehatananak/data/repository/AktivitasRepository.kt
@@ -0,0 +1,19 @@
+package com.abcd.pemantauankesehatananak.data.repository
+
+import com.abcd.pemantauankesehatananak.data.database.api.ApiService
+import com.abcd.pemantauankesehatananak.data.model.AktivitasModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AktivitasRepository @Inject constructor(
+    private val api: ApiService
+) {
+    suspend fun getAktivitas(
+        idUser: Int, umur: Int
+    ):ArrayList<AktivitasModel>{
+        return api.getAktivitas("", idUser, umur)
+    }
+
+}
---
+++ app/src/main/java/com/abcd/pemantauankesehatananak/ui/fragment/user/rekomendasi/AktivitasViewModel.kt
@@ -0,0 +1,36 @@
+package com.abcd.pemantauankesehatananak.ui.fragment.user.rekomendasi
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.abcd.pemantauankesehatananak.data.model.AktivitasModel
+import com.abcd.pemantauankesehatananak.data.repository.AktivitasRepository
+import com.abcd.pemantauankesehatananak.utils.network.UIState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AktivitasViewModel @Inject constructor(
+    private val repository: AktivitasRepository
+):ViewModel() {
+    private val _aktivitas = MutableLiveData<UIState<ArrayList<AktivitasModel>>>()
+    val getAktivitas : LiveData<UIState<ArrayList<AktivitasModel>>> = _aktivitas
+
+    fun loadData(
+        idUser:Int, umur:Int
+    ){
+        viewModelScope.launch {
+            try {
+                _aktivitas.postValue(UIState.Loading)
+                delay(1_000)
+                val data = repository.getAktivitas(idUser, umur)
+                _aktivitas.postValue(UIState.Success(data))
+            } catch (ex: Exception) {
+                _aktivitas.postValue(UIState.Failure("Error: ${ex.message}"))
+            }
+        }
+    }
+}
