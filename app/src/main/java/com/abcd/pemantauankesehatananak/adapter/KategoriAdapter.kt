5d4d5c868f7ba9164e86667dd9152cd01867d2b7
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-06-24 13:17:08 +0800

    add kategori and link to milestones

--- app/src/main/java/com/abcd/pemantauankesehatananak/adapter/KategoriAdapter.kt
+++ app/src/main/java/com/abcd/pemantauankesehatananak/adapter/KategoriAdapter.kt
@@ -2,7 +2,10 @@ package com.abcd.pemantauankesehatananak.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.abcd.pemantauankesehatananak.R
 import com.abcd.pemantauankesehatananak.data.model.KategoriModel
 import com.abcd.pemantauankesehatananak.databinding.ItemListKategoriBinding
 import com.abcd.pemantauankesehatananak.utils.OnClickItem
@@ -11,6 +14,7 @@ class KategoriAdapter(
     private val listKategori: ArrayList<KategoriModel>,
     private val onClick: OnClickItem.ClickKategori,
 ) : RecyclerView.Adapter<KategoriAdapter.KategoriViewHolder>() {
+    private var selectedPosition = 0
 
     inner class KategoriViewHolder(val binding: ItemListKategoriBinding) : RecyclerView.ViewHolder(binding.root)
 
@@ -25,14 +29,34 @@ class KategoriAdapter(
         holder.apply {
             binding.apply {
                 tvJudul.text = kategori.kategori
+                val isSelected = position == selectedPosition
+
+                if (isSelected) setActiveData(binding) else setInactiveData(binding)
+
             }
             itemView.setOnClickListener {
+                selectedPosition = holder.getAdapterPosition()
+
+                notifyDataSetChanged()
                 onClick.clickKategori(kategori)
             }
         }
-
     }
 
     override fun getItemCount() = listKategori.size
+
+    fun setActiveData(binding: ItemListKategoriBinding){
+        binding.apply {
+            clKategori.setBackgroundResource(R.drawable.bg_card_active)
+            tvJudul.setTextColor(ContextCompat.getColor(binding.root.context, R.color.white))
+        }
+    }
+
+    fun setInactiveData(binding: ItemListKategoriBinding){
+        binding.apply {
+            clKategori.setBackgroundResource(R.drawable.bg_card)
+            tvJudul.setTextColor(ContextCompat.getColor(binding.root.context, R.color.black))
+        }
+    }
 
 }
