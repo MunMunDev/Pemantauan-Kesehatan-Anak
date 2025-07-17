32e5354ff80b7293cab3f19f959ab942f8246911
MunMunDev <144525374+MunMunDev@users.noreply.github.com>
2025-07-17 23:36:19 +0800

    menambahkan detail pemeriksaan. mengatur ui dan menampilkan data yang telah diambil dari intent data sebelumnya

--- app/src/main/java/com/abcd/pemantauankesehatananak/ui/activity/pemeriksaan/DetailPemeriksaanActivity.kt
+++ app/src/main/java/com/abcd/pemantauankesehatananak/ui/activity/pemeriksaan/DetailPemeriksaanActivity.kt
@@ -1,15 +1,48 @@
 package com.abcd.pemantauankesehatananak.ui.activity.pemeriksaan
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.abcd.pemantauankesehatananak.R
+import com.abcd.pemantauankesehatananak.data.model.PelayananModel
+import com.abcd.pemantauankesehatananak.databinding.ActivityDetailPemeriksaanBinding
 
+@Suppress("DEPRECATION")
 class DetailPemeriksaanActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailPemeriksaanBinding
+    private var pelayanan: PelayananModel? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_pemeriksaan)
+        binding = ActivityDetailPemeriksaanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setDataSebelumnya()
+        setTopBar()
     }
+
+    private fun setDataSebelumnya() {
+        val i = intent
+        if(i != null){
+            pelayanan = i.getParcelableExtra("pemeriksaan")
+            setData(pelayanan)
+        }
+    }
+
+    private fun setTopBar() {
+        binding.myAppBar.apply {
+            tvTitle.text = pelayanan?.pelayanan
+            ivNavDrawer.visibility = View.GONE
+            ivBack.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setData(pelayanan: PelayananModel?) {
+        pelayanan?.let {data->
+            binding.apply {
+                tvHasil.text = data.hasil
+                tvKeterangan.text = data.keterangan
+                tvCatatan.text = data.catatan
+            }
+        }
+    }
+
 }
